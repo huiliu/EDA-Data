5cd989e9b18a352c194893560c59765b36b37a43
liuhui <hui.liu.e@gmail.com>
2011-04-24 05:08:04 +0800

    Contained a function used to matrix transpose operation

---
+++ matrix.py
@@ -0,0 +1,24 @@
+#!/export/home/liuhui/opt/python3/bin/python3
+
+from pprint import pprint
+
+
+def transpose( matrix ):
+    nColumn = len(matrix[0])
+    nRow = len(matrix)
+#    print('nRow=%d nColumn=%d'%(nRow,nColumn))
+#pprint(matrix)
+    trans = []
+    i = 0
+    while i < nColumn:
+        j = 0
+        tmp = []
+        while j < nRow:
+#print('i=%d j=%d'%(i,j))
+            tmp.append( matrix[j][i] )
+            j += 1
+        trans.append(tmp)
+        i += 1
+#    print( 'nRow = %d, nColumn = %d'%(len(trans), len(trans[0])))
+    return trans
+
