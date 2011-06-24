52117323f3e05cf34d8065cfca2a4c5d9b783508
liuhui@pauling <hui.liu.e@gmail.com>
2011-06-24 22:23:17 +0800

    Delete blank line

--- offset.py
+++ offset.py
@@ -69,7 +69,7 @@ def Distance(coord1, coord2):
         i += 1
 
     return math.sqrt( rr[0]*rr[0] + rr[1]*rr[1] + rr[2]*rr[2] )
-        
+
 def GetTwoBlock(blocks, i, j):
     """
         GenerateTable   get the block that you need
