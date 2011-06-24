55f1c7257649862e53e406c3de3b98cdd98aaf84
liuhui@pauling <hui.liu.e@gmail.com>
2011-06-24 22:19:37 +0800

    eliminate.py have been able to handled the lcd file which contain "*"

--- eliminate.py
+++ eliminate.py
@@ -1,53 +1,80 @@
 #!/export/home/liuhui/opt/bin/python3
 
 """
-    This script use to 
+    This script use to handle the lcd file which contain "*"
+    The input file is lcd file
 """
 
 import common
-from pprint import pprint
+import sys
 import re
+from pprint import pprint
 
-#read data from the file which contain '*', and split to list
-data = common.SplitToBlock( common.ReadDataFromFile('fix43-eda.lcd') )
+def getCoord( fName ):
+    #read data from the file which contain '*', and split to list
+    data = common.SplitToBlock( common.ReadDataFromFile(fName) )
 
-#get coordination
-aa = []
-reg = re.compile(' DU ')
-for x in data :
-    for y in x:
-        if reg.match( y ):
-#           prfloat( y )
-            aa.append( x )
-            break
+    #get coordination
+    refined_Coord = []
+    # Use regular expression to match the coordiantion ofLCD
+    reg = re.compile(' DU ')
+    for x in data :
+        for y in x:
+            if reg.match( y ):
+                refined_Coord.append( x )
+                break
+    return refined_Coord
 
-def DataList( larray, i ):
+def Cut( coordBlock ):
+    """
+        Cut             delete the string "DU"
+    """
     tmp = []
-    for x in larray[i]:
-        tmp.append( x.strip().split()[1:] )
-    return tmp
+    block = []
+
+    for  larray in coordBlock:
+        for x in larray:
+            #tmp.append( x.strip().split()[1:] )
+            tmp.append( x.strip().split() )
+        block.append( tmp )
+        tmp =[]
+    return block
+
+def Adjust( org, comp ):
+    correct = org
+    for x in correct:
+        for z in  comp:
+            if x[1] == z[1] or x[3] == z[3]:
+                x[1] = z[1]
+                x[2] = z[2]
+                x[3] = z[3]
+    #           x[0] = float(x[0])
+    #           x[1] = float(x[1])
+    #           x[2] = float(x[2])
+    return correct
+
+def refine( fName ):
+
+    lcd_coord = Cut( getCoord( fName ) )
+
+    supermolecule = lcd_coord[4]
+    monomer1 = lcd_coord[5]
+    monomer2 = lcd_coord[6]
+
+    lcd_coord[5] = Adjust( monomer1, supermolecule )
+    lcd_coord[6] = Adjust( monomer2, supermolecule )
+#   pprint( lcd_coord )
+    out(lcd_coord)
+
+def out(coord):
+    for x in coord:
+        for y in x:
+            for z in y:
+                print( z.center(12), end='' )
+            print()
+        print()
 
 
-supermolecule = DataList(aa,4)
-monomer1 = DataList(aa,5)
-monomer2 = DataList(aa,6)
-
-#pprint(supermolecule)
-#pprint( monomer1)
-#pprint( monomer2)
-
-for x in monomer1:
-    for z in  supermolecule:
-        if x[0] == z[0]:
-            x[1] = z[1]
-            x[0] = float(x[0])
-            x[1] = float(x[1])
-            x[2] = float(x[2])
-
-for y in monomer2:
-    for z in  supermolecule:
-        if y[0] == z[0]:
-            y[1] = z[1]
-
-pprint(monomer1)
-pprint(monomer2)
+if __name__ == '__main__':
+#   print(sys.argv[1])
+    refine( sys.argv[1] )
